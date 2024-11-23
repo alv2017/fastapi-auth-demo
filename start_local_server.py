dbe2b06cd81104a82b35cea6cf3c336f49dc9b87
alv2017 <v.alishauskaite@gmail.com>
2024-11-23 22:20:09 +0200

    files supporting application debugging has been added

---
+++ start_local_server.py
@@ -0,0 +1,8 @@
+# created for debugging purposes
+
+import uvicorn
+from app.main import app
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8888)
---
+++ start_running_tests.py
@@ -0,0 +1,8 @@
+# created for debugging purposes
+
+import sys
+import pytest
+
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["-svv", "tests"]))
